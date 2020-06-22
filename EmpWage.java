54274c3f6a28f7d0bdd8e8c66a8847799b1d4f79
sonvin2916 <sonalchougule07@gmail.com>
2020-06-22 13:23:52 +0530

    UC-1 : Added

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,15 @@ public class EmpWage
 {
 	public static void main(String args[])	
 	{
-		System.out.println("Welcome");
+		int isPresent = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent)
+		{	
+			 System.out.println("Employee is Present");
+		}	
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }	
